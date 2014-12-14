15f8b46a6366886ebae2fa879c5c5da3023a1ed6
baronvonbadguy <baronvonbadguy>
2014-12-14 02:03:55 -0500

    added pre-existing render check

--- render.py
+++ render.py
@@ -20,33 +20,38 @@ def vfc_from_file(filename, directory):
 
 
 def render_vines(data):
+    #verify files exist in cache folder
     datav = exists(data, 'cache')
+    datavrid = exists(data, 'render')['id'].astype(basestring)
     for i, vineid in enumerate(datav['id'].astype(basestring)):
-        vine = vfc_from_file(vineid, 'cache').on_color(size=(854, 480),
-                                                       color=(20, 20, 25),
-                                                       pos='center')
-        #encodes text as ascii for textclip creation
-        user = data['username'].astype(basestring)[i].encode('ascii', 'ignore')
-        desc = data['description'].astype(basestring)[i].encode('ascii', 'ignore')
-        user = 'Vine By:' + user
-        user_osd = (mpe.TextClip(txt=user, size=(187, 480),
-                                 method='caption', align='center',
-                                 font='arial', fontsize=40,
-                                 color='white', interline=20)
-                    .set_duration(vine.duration))
-        desc_osd = (mpe.TextClip(txt=desc, size=(187, 480),
-                                 method='caption', align='center',
-                                 font='arial', fontsize=28,
-                                 color='white')
-                    .set_duration(vine.duration)
-                    .set_pos('right'))
-        #composite the text on the sides of the video
-        comp = mpe.CompositeVideoClip([vine, user_osd, desc_osd])
-        render_path = ap('render/' + vineid + '.mp4')
-        #start the render
-        comp.write_videofile(render_path, fps=30,
-                             codec='libx264', threads=2,
-                             verbose=True)
+        if vineid not in datavrid:
+            vine = vfc_from_file(vineid, 'cache').on_color(size=(854, 480),
+                                                           color=(20, 20, 25),
+                                                           pos='center')
+            #encodes text as ascii for textclip creation
+            user = data['username'].astype(basestring)[i].encode('ascii', 'ignore')
+            desc = data['description'].astype(basestring)[i].encode('ascii', 'ignore')
+            user = 'Vine By:' + user
+            user_osd = (mpe.TextClip(txt=user, size=(187, 480),
+                                     method='caption', align='center',
+                                     font='arial', fontsize=40,
+                                     color='white', interline=20)
+                        .set_duration(vine.duration))
+            desc_osd = (mpe.TextClip(txt=desc, size=(187, 480),
+                                     method='caption', align='center',
+                                     font='arial', fontsize=28,
+                                     color='white')
+                        .set_duration(vine.duration)
+                        .set_pos('right'))
+            #composite the text on the sides of the video
+            comp = mpe.CompositeVideoClip([vine, user_osd, desc_osd])
+            render_path = ap('render/' + vineid + '.mp4')
+            #start the render
+            comp.write_videofile(render_path, fps=30,
+                                 codec='libx264', threads=2,
+                                 verbose=True)
+        else:
+            print('skipping ' + vineid)
 
 
 def concat_vines(data):
