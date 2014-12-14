c12424c5c0071858cf53f715c78c5dadd7842cb2
baronvonbadguy <baronvonbadguy>
2014-12-14 01:13:45 -0500

    added group rendering

--- render.py
+++ render.py
@@ -8,36 +8,73 @@ Created on Thu Dec 11 14:36:33 2014
 import pandas as pd
 from scraper import append_path
 from moviepy import editor as mpe
+from os import path as osp
+
+
+#checks all the id's of the vines to see if there is a corresponding file
+#in the specified directory
+def vine_exists(data, directory):
+    if directory in ['cache', 'render']:
+        #filter lambda for the dataframe
+        is_file = lambda vineid: osp.isfile(append_path(directory + '/' + str(vineid) + '.mp4'))
+        datav = data[data['id'].map(is_file)]
+        return datav
+    else:
+        return pd.DataFrame() 
+ 
+def group_data(data, group_size):
+    return [data[x:x+group_size] for x in range(0, len(data), group_size)]
+
+
+def vfc_from_file(indentifier, directory):
+    return mpe.VideoFileClip(append_path(str(directory) + '/' + str(identifier) + '.mp4'))
 
 
 def render_vines(data):
-    for i, vineid in enumerate(data['id'].astype(basestring)):
-        #gets absolute path of video
-        vine_path = append_path('cache/' + vineid + '.mp4')
-        #creates vine VFC and formats it
-        vine = mpe.VideoFileClip(vine_path)
-        vine = vine.on_color(size=(854, 480), color=(20, 20, 25),
-                             pos='center')
+    datav = vine_exists(data, 'cache')
+    for i, vineid in enumerate(datav.astype(basestring)):
+        vine = vfc_from_vine(vineid, 'cache').on_color(size=(854, 480),
+                                                       color=(20, 20, 25),
+                                                       pos='center')
         #encodes text as ascii for textclip creation
         user = data['username'].astype(basestring)[i].encode('ascii', 'ignore')
         desc = data['description'].astype(basestring)[i].encode('ascii', 'ignore')
+        user = 'Vine By:' + user
         user_osd = (mpe.TextClip(txt=user, size=(187, 480),
                                  method='caption', align='center',
-                                 font='arial', fontsize=30,
-                                 color='white')
+                                 font='arial', fontsize=40,
+                                 color='white', interline=20)
                     .set_duration(vine.duration))
         desc_osd = (mpe.TextClip(txt=desc, size=(187, 480),
                                  method='caption', align='center',
-                                 font='arial', fontsize=24,
+                                 font='arial', fontsize=28,
                                  color='white')
-                    .set_duration(vine.duration))
+                    .set_duration(vine.duration)
+                    .set_pos('right'))
         #composite the text on the sides of the video
-        comp = mpe.CompositeVideoClip([vine, user_osd, desc_osd.set_pos('right')])
+        comp = mpe.CompositeVideoClip([vine, user_osd, desc_osd])
         render_path = append_path('render/' + vineid + '.mp4')
+        #start the render
         comp.write_videofile(render_path, fps=30,
                              codec='libx264', threads=2,
                              verbose=True)
 
+
+def concat_vines(data):
+    datavid = vine_exists(data, 'render')['id']
+    groups = group_data(datavid, 10)
+    #lambda to get video from group number
+    vfcg = lambda group: vfc_from_file('group_' + str(group), 'render/groups')
+    group_render_path = lambda f: append_path('render/groups/' + f + '.mp4')
+    for i, group in enumerate(groups):
+        videos = [vfc_from_file(vineid, 'render') for vineid in group]
+        concat = mpe.concatenate_videoclips(videos)
+        concat.write_videofile(group_render_path('group_' + str(i)))
+    videos = [vfcg(groupid) for groupid in range(len(groups))]
+    concat = mpe.concatenate_videoclips(group_render_path('FINAL RENDER'))
+
+
 if __name__ == '__main__':
     data = pd.read_csv(append_path('records.csv'), encoding='utf-8')
     render_vines(data)
+    concat_vines(data)
