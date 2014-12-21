041e0b92563659fbf082cb18ce4b20c347a09b85
baronvonbadguy <baronvonbadguy>
2014-12-21 13:17:20 -0500

    removed rendering threads due to memory leak

--- render.py
+++ render.py
@@ -28,8 +28,6 @@ def render_vines(data):
     datav = exists(data, 'cache')
     #files already rendered get skipped
     datavrid = list(exists(data, 'render')['id'].astype(basestring))
-    #q = Queue()
-    #thread_pool(q, 1, ThreadWritex264)
     for i, row in datav.iterrows():
         vineid = row['id']
         if vineid not in datavrid:
@@ -37,9 +35,9 @@ def render_vines(data):
                                                            color=(20, 20, 25),
                                                            pos='center')
             #encodes text as ascii for textclip creation
-            user = enc_str(row['username'])
-            desc = enc_str(row['description'])
-            user = 'Vine By:\n' + user
+            user = enc_str(row['username']).upper()
+            desc = enc_str(row['description']).upper()
+            user = 'VINE BY:\n' + user
             #lambda to create text clip
             tc = lambda text, size, xline: (mpe.TextClip(txt=text, size=(180, 480),
                                             method='caption', align='center',
@@ -51,18 +49,16 @@ def render_vines(data):
             #composite the text on the sides of the video
             comp = mpe.CompositeVideoClip([vine, user_osd, desc_osd])
             #start the render
-            #q.put((comp, ap('render/' + vineid + '.mp4')))
             path = ap('render/' + vineid + '.mp4')
             comp.write_videofile(path, codec='libx264',
                                  threads=4, verbose=True, fps=30)
         else:
             print('skipping ' + vineid)
-    #q.join()
 
 
 def concat_vines(data):
     datavid = exists(data, 'render')['id']
-    groups = group_data(datavid, 10)
+    groups = group_data(datavid, 20)
     if not osp.isdir(ap('render/groups')):
         os.makedirs('render/groups')
     #lambda to get VideoFileClip from group number
